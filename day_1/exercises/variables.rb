e99054d2850e1eeba241ec6d58a2d7d06c3078c8
jackson-r <stellers.jack@gmail.com>
2018-10-22 20:36:45 -0600

    finished day 1

--- day_1/exercises/variables.rb
+++ day_1/exercises/variables.rb
@@ -5,16 +5,17 @@
 # example: Write code below to save your name to a variable and
 # print that variable to the terminal:
 name = "Harry Potter"
-p name
+p "Jeff Jeff"
 
 # Write code below to save the string 'Dobby' to a variable and
 # print that variable to the terminal:
 house_elf = "Dobby"
-#YOUR CODE HERE
+p house_elf
 
 # Write code below to save the string 'Harry Potter must not return to Hogwarts!'
 # and print that variable to the terminal:
-# YOUR CODE HERE
+unreasonable_demand = "Harry Potter must not return to Hogwarts!"
+p unreasonable_demand
 
 # example: Write code below to add 2 to the variable `students` and
 # print the result:
@@ -24,5 +25,6 @@ p students
 
 # Write the code below to subract 2 students from the `students` variable and
 # print the result:
-#YOUR CODE HERE
+students -= 2
+
 p students
