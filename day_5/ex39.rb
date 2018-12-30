6d9b7d5e23050e89443c18a714ebde617ab3dc09
jackson-r <stellers.jack@gmail.com>
2018-12-29 22:22:11 -0700

    Finished day 5

--- day_5/ex39.rb
+++ day_5/ex39.rb
@@ -7,7 +7,7 @@ states =  {
   'Florida' => 'FL',
   'California' => 'CA',
   'New York' => 'NY',
-  'Michigag' => 'MI'
+  'Michigan' => 'MI'
 }
 
 # create a basic set of states and some cities in them
@@ -33,6 +33,79 @@ puts "Florida's abbreviation is: #{states['Florida']}"
 
 # do it by using the state then cities dict
 puts '-' * 10
-puts "Michigan has: #{cities[states['Michigan'
+puts "Michigan has: #{cities[states['Michigan']]}"
+puts "Florida has: #{cities[states['Florida']]}"
 
+# puts every state abbreviation
+puts '-' * 10
+states.each do |state, abbrev|
+  puts "#{state} is abbreviated #{abbrev}"
+end
+
+# puts every city in state
+puts '-' * 10
+cities.each do |abbrev, city|
+  puts "#{abbrev} has the city #{city}"
+end
+
+# now do both at the same time
+puts '-' * 10
+states.each do |state, abbrev|
+  city = cities[abbrev]
+  puts "#{state} is abbreviated by #{abbrev} and has city #{city}"
+end
+
+puts '-' * 10
+# by default ruby says "nil" when something isn't there
+state = states['Texas']
+
+if !state
+  puts "Sorry, no Texas."
+end
+
+# default values using ||= with the nil result
+city =  cities['TX']
+city ||= 'Does Not Exist'
+puts "The city for the state 'TX' is: #{city}"
+
+
+
+
+puts '-' * 10
+# Study Drills:  do this mapping with another country
+
+provinces = {
+  'Alberta' => 'AB',
+  'British Columbia' => 'BC',
+  'Manitoba' => 'MB',
+  'Nova Scotia' => 'NS',
+  'Ontario' => 'ON',
+  'Quebec' => 'QC',
+  'Saskatchewan' => 'SK'
+}
+
+puts "Here are some of the provinces in Canadad and their abbreviations."
+provinces.each do |province, abbr|
+  puts "#{province} is abbreviated #{abbr}"
+end
+
+capitals = Hash.new
+capitals['AB'] = 'Edmonton'
+capitals['BC'] = 'Victoria'
+capitals['MB'] = 'Winnipeg'
+capitals['ON'] = 'Toronto'
+capitals['QC'] = 'Quebec City'
+capitals['Saskatchewan'] = 'Regina'
+
+puts "Here are some of the capitals in Canada."
+capitals.each do |abbr, capital|
+  puts "#{capital} is the capital of #{abbr}"
+end
+
+# Add a default value that is not nil for keys not in the hash.
+# Merge a new hash with the values on an old.
+
+canada_capitals = Hash.new("No Province with that abbreviation").merge(capitals)
+puts "The capital of Quebec is #{canada_capitals['QC']}"
+puts "The captial of New Mexico is #{canada_capitals['NM']}"
 
--- day_5/questions.md
+++ day_5/questions.md
@@ -1,13 +1,31 @@
 ## Day 5 Questions
 
 1. What is a Hash, and how is it different from an Array in Ruby?
+  A hash is a set of matched arrays, usually used for storing things that come in pairs,
+  and designed to allow reference to a key(typically string or symbol) instead of an
+  index.  Yay syntactic sugar!
+  The big difference is the use of a key instead of an index to get values.
 
 1. In the space below, create a Hash stored to a variable named `pet_store`.  This hash should hold an inventory of items and the number of that item that you might find at a pet store.
+  pet_store = {bouncy_balls: 37, dog_food: 14, cat_food: 20, FANCY_cat_food: 7}
 
 1. given the following `states = {"CO" => "Colorado", "IA" => "Iowa", "OK" => "Oklahoma"}`, how would you access the value `"Iowa"`?
+  states['IA']
 
 1. With the same hash above, how would we get all the keys?  All the values?
+  states.keys
+  states.values
 
 1. What is another example of when we might use a hash?  In this case, why is a hash better than an array?
-
+  User names and passwords (NEVER actually do this)
+  I think you would store class variables  in a hash: person.new attr = {hair: blonde}
+  Synatactic Sugar!  If I write sensible names and symbols, reading off the value of my
+  reads like english, and is much MUCH easier to keep track of. 
+  
 1. What questions do you still have about hashes?
+  How do you 'break' a hash / what unexpected behaviour do hashes have?
+    <em>see destroyallsoftware.com/talks/wat<em>  (assign a to a with a undefined = nil)
+  Could I create a Hash3 class that has two values per key, and would that be silly/why?
+  In the Ruby documentation you can use a Class as a hash key, but I don't understand 
+  why it takes that extra ...stuff. 
+
