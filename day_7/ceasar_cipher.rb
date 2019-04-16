ac6cea603f208688159b0ae34a66137864337960
jackson-r <stellers.jack@gmail.com>
2019-04-15 22:12:31 -0600

    started checker_board

--- day_7/ceasar_cipher.rb
+++ day_7/ceasar_cipher.rb
@@ -2,32 +2,42 @@
 #  Encodes message in a shifted alphabet
 
 
-# cipher = ceaser_cipher.new
-# cipher.encode("text", 3)
 # I guess I could also do this with a hash?
 
 
-# def ceaser_cipher(text, shift)
-#   alpha = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-#   shift_alpha = alpha
-#   # will be shifted to the right
-#   shift.times do 
-#     shift_alpha << shift_alpha.shift
-#   end
-#   text.gsub!(/./) do |letter| 
-#     letter = alpha[shift_alpha.find_index(letter)]
-#     p alpha[shift_alpha.find_index(letter)]
-#   end
-#   return text
-# end
-# 
-# puts ceaser_cipher("abcde", 2)
 
-text = "abcde"
-alpha = ['a','b','c','d','e']
-shift_a = alpha
-shift_a << shift_a.shift
-puts shift_a
-text.gsub!(/./) { |n| n = alpha[shift_a.find_index(n)] }
-puts text
+
+class Ceaser_cipher
+  def initialize(shift = 1)
+    @shifted = shift
+    @alpha = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+  end
+
+  def encript(text)
+    shifted_alpha = Array.new(@alpha)
+    @shifted.times do
+      shifted_alpha << shifted_alpha.shift
+    end
+    text.gsub!(/./) do |letter| 
+      letter = @alpha[shifted_alpha.find_index(letter)]
+    end
+    return text
+  end
+
+  def decript(text)
+    unshifted_alpha = Array.new(@alpha)
+    @shifted.times do
+      unshifted_alpha.unshift(unshifted_alpha.pop)
+    end
+    text.gsub!(/./) do |letter|
+      letter = @alpha[unshifted_alpha.find_index(letter)]
+    end
+    return text
+  end
+end
+
+
+cipher = Ceaser_cipher.new(3)
+p cipher.encript("hello")
+p cipher.decript(cipher.encript("hello"))
 
