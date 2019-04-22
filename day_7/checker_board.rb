67c99575148ba9e00b3ae68dd4d37e5034c011d3
jackson-r <stellers.jack@gmail.com>
2019-04-22 09:11:22 -0400

    working on checker

---
+++ day_7/checker_board.rb
@@ -0,0 +1,32 @@
+#  CheckerBoard
+
+
+class Checker_board(width = 3, height = 3, invert = FALSE)
+  def initialize
+    @width = width
+    @height = height
+    @invert = invert
+    # possibly add attr reader and change methods later
+
+    # add invert conditional to toggle
+    toggle = 0
+    h = Array.new(@height)
+    w = Array.new(@width)
+    for i in 1..h.length
+      if toggle == 0
+        for ii in 1..w.length
+
+        end
+        toggle = 1
+      elsif toggle = 1
+        for ii in
+        end
+      else
+        #exception
+      end
+    end
+  end
+
+end
+
+
