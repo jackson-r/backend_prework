b24ac0ae77bc5066dd5cdd2f8a9ca51c42a5e689
jackson-r <stellers.jack@gmail.com>
2019-04-23 21:11:47 -0400

    working on checkerboard Class def

--- day_7/checker_board.rb
+++ day_7/checker_board.rb
@@ -1,32 +1,49 @@
 #  CheckerBoard
 
 
-class Checker_board(width = 3, height = 3, invert = FALSE)
-  def initialize
+class Checker_board
+  attr_reader :board
+  def initialize(width = 3, height = 3, invert = false)
     @width = width
     @height = height
     @invert = invert
+    @board
     # possibly add attr reader and change methods later
+  end
 
-    # add invert conditional to toggle
-    toggle = 0
-    h = Array.new(@height)
-    w = Array.new(@width)
-    for i in 1..h.length
-      if toggle == 0
-        for ii in 1..w.length
-
-        end
-        toggle = 1
-      elsif toggle = 1
-        for ii in
-        end
-      else
-        #exception
-      end
+  def generate(width, height, invert)
+    @width = width
+    @height = height
+    @invert = invert
+    if invert == true
+      toggle = 1
+    else
+      toggle = 0
     end
+    checkerboard = String.new
+    for i in 1..h.length
+      for ii in 1..w.length
+        if toggle%2 == 0
+          checkerboard += "#"
+          toggle += 1
+          p toggle%2
+        elsif toggle%2 == 1
+          checkerboard += " "
+          toggle += 1
+          p toggle%2
+        end
+      end
+      checkerboard +="\n" 
+      toggle += 2
+      p toggle%2
+    end
+    puts checkerboard
+    @board = checkerboard
   end
 
 end
 
+Checker_board.new
+Checker_board.board
 
+Checker_board.generate
