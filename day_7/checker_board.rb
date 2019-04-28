4e641727ac49f52d92dc05d52362cb4178515efa
jackson-r <stellers.jack@gmail.com>
2019-04-28 10:08:23 -0600

    finished all the projects

--- day_7/checker_board.rb
+++ day_7/checker_board.rb
@@ -4,46 +4,51 @@
 class Checker_board
   attr_reader :board
   def initialize(width = 3, height = 3, invert = false)
-    @width = width
-    @height = height
+    @width = width.to_i
+    @height = height.to_i
     @invert = invert
-    @board
+    @board = String.new
     # possibly add attr reader and change methods later
-  end
 
-  def generate(width, height, invert)
-    @width = width
-    @height = height
-    @invert = invert
-    if invert == true
+    # print "#" on even, " " on odd.  incrementing toggle by 1 will invert the board.
+    if invert == true  || invert == 'invert' || invert == 1
       toggle = 1
     else
       toggle = 0
     end
     checkerboard = String.new
-    for i in 1..h.length
-      for ii in 1..w.length
+    for i in 1..@height
+      for ii in 1..@width
         if toggle%2 == 0
           checkerboard += "#"
           toggle += 1
-          p toggle%2
+          # p toggle
         elsif toggle%2 == 1
           checkerboard += " "
           toggle += 1
-          p toggle%2
+          # p toggle
         end
       end
       checkerboard +="\n" 
-      toggle += 2
-      p toggle%2
+      toggle += 1
+      # p toggle
     end
-    puts checkerboard
+    #puts checkerboard
     @board = checkerboard
+  end
+
+  def display
+    puts @board
   end
 
 end
 
-Checker_board.new
-Checker_board.board
+p "4x4 board"
+newboard = Checker_board.new(4, 4)
+newboard.display
 
-Checker_board.generate
+p "-"*20
+
+p "\n8x6 board, inverted"
+newboard2 = Checker_board.new(8, 6, "invert")
+newboard2.display
